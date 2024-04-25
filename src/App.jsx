9dac814da175aaeba24c2590bfc5fce3fe75533b
Jessica <125924091+jessicasantosb@users.noreply.github.com>
2024-04-25 10:35:22 -0300

    Feat: bootstrappping Home and About pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
-import './App.css'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <BrowserRouter>
+      <header>
+        <Link className='site-logo' to={'/'}>
+          #VanLife
+        </Link>
+
+        <nav>
+          <Link to={'/'}>Home</Link>
+          <Link to={'/about'}>About</Link>
+        </nav>
+      </header>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
