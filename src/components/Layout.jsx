e0f162608bce8601dd0dfaac9d81cbc007b6299b
Jessica <125924091+jessicasantosb@users.noreply.github.com>
2024-04-25 20:13:43 -0300

    Feat: create Footer component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
+import Footer from './Footer';
 import Header from './Header';
 
 function Layout() {
@@ -9,6 +10,7 @@ function Layout() {
       <main>
         <Outlet />
       </main>
+      <Footer />
     </>
   );
 }
