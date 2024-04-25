8c8b5cdced8eb367ce1cb5c0d8d97548f5326ed4
Jessica <125924091+jessicasantosb@users.noreply.github.com>
2024-04-25 14:11:02 -0300

    Feat: nest all routes in a Layout element

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,23 @@
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './pages/About';
 import Home from './pages/Home';
 import VanDetail from './pages/VanDetail';
 import Vans from './pages/Vans';
 
+import Layout from './Layout';
 import './server/server';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className='site-logo' to={'/'}>
-          #VanLife
-        </Link>
-
-        <nav>
-          <Link to={'/'}>Home</Link>
-          <Link to={'/about'}>About</Link>
-          <Link to={'/vans'}>Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/vans' element={<Vans />} />
-        <Route path='/vans/:id' element={<VanDetail />} />
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/vans' element={<Vans />} />
+          <Route path='/vans/:id' element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
