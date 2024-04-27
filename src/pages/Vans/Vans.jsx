f114c45663d41b6d7de36ae8f92282bc7c1569e6
Jessica <125924091+jessicasantosb@users.noreply.github.com>
2024-04-26 21:09:13 -0300

    Feat: add a filter function to Vans

--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 
 function Vans() {
   const [vans, setVans] = React.useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const paramsType = searchParams.get('type');
+
+  const displayedVans = paramsType ? vans.filter(({ type }) => type === paramsType) : vans;
+
   React.useEffect(() => {
     fetch('/api/vans')
       .then((res) => res.json())
@@ -12,8 +18,34 @@ function Vans() {
   return (
     <div className='van-list-container'>
       <h1>Explore our van options</h1>
+      <div className='van-list-filter-buttons'>
+        <button
+          onClick={() => setSearchParams({ type: 'simple' })}
+          className={`van-type simple ${paramsType === 'simple' && 'selected'}`}
+        >
+          simple
+        </button>
+        <button
+          onClick={() => setSearchParams({ type: 'rugged' })}
+          className={`van-type rugged ${paramsType === 'rugged' && 'selected'}`}
+        >
+          rugged
+        </button>
+        <button
+          onClick={() => setSearchParams({ type: 'luxury' })}
+          className={`van-type luxury ${paramsType === 'luxury' && 'selected'}`}
+        >
+          luxury
+        </button>
+        {paramsType && (
+          <button onClick={() => setSearchParams({})} className='van-type clear-filters'>
+            clear
+          </button>
+        )}
+      </div>
+
       <div className='van-list'>
-        {vans.map(({ id, name, imageUrl, price, type }) => (
+        {displayedVans.map(({ id, name, imageUrl, price, type }) => (
           <div key={id} className='van-tile'>
             <Link to={id}>
               <img src={imageUrl} />
