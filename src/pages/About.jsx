5fa682a5e29710f3c6b4f460a056e3eca3edfbe0
Jessica <125924091+jessicasantosb@users.noreply.github.com>
2024-05-13 14:44:28 -0300

    Style: refactor About page layout

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,26 +1,43 @@
 import React from 'react';
-import hero from '../assets/about-hero.png';
 import { Link } from 'react-router-dom';
+import hero from '../assets/about-hero.png';
+import styles from './About.module.css';
 
 function About() {
   return (
-    <section className='about-page-container'>
-      <img src={hero} alt='about hero' className='about-hero-image' />
-      <div className='about-page-content'>
-        <h1>Don’t squeeze in a sedan when you could relax in a van.</h1>
-        <p>
-          Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before
-          each trip to ensure your travel plans can go off without a hitch. (Hitch costs extra 😉)
-        </p>
-        <p>Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.</p>
+    <section className={styles.container}>
+      <div className={styles.hero}>
+        <h1 className='title'>Why are we?</h1>
       </div>
-      <div className='about-page-cta'>
-        <h2>
+
+      <div className={styles.content}>
+        <h1 className='title'>
+          Don’t squeeze in a sedan when you could relax in a van.
+        </h1>
+        <div className={styles.contentText}>
+          <img src={hero} alt='about hero' />
+          <div>
+            <p>
+              Our mission is to enliven your road trip with the perfect travel
+              van rental. Our vans are recertified before each trip to ensure
+              your travel plans can go off without a hitch. (Hitch costs extra
+              😉)
+            </p>
+            <p>
+              Our team is full of vanlife enthusiasts who know firsthand the
+              magic of touring the world on 4 wheels.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.cta}>
+        <h2 className='title'>
           Your destination is waiting.
           <br />
           Your van is ready.
         </h2>
-        <Link className='link-button' to='/vans'>
+        <Link className={styles.ctaButton} to='/vans'>
           Explore our vans
         </Link>
       </div>
