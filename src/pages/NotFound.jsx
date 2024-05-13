36c56d61833787423e607bdd579745863372a51e
Jessica <125924091+jessicasantosb@users.noreply.github.com>
2024-05-13 15:20:10 -0300

    Style: refactor Not Found page layout

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styles from './NotFound.module.css'
 
 function NotFound() {
   return (
-    <section className='not-found-container'>
-      <h1>Sorry, the page you were looking for was not found.</h1>
-      <Link to='/' className='link-button'>
+    <section className={styles.container}>
+      <h1 className='title'>Not Found</h1>
+      <p>sorry, the page you were looking for was not found.</p>
+      <Link to='/' className={styles.button}>
         Return to Home
       </Link>
     </section>
