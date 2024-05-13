ff64af9a7c34a3faf287079093ec21f1522d84c5
Jessica <125924091+jessicasantosb@users.noreply.github.com>
2024-05-13 15:12:47 -0300

    Style: refactor Login page layout

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,25 +1,36 @@
 import React from 'react';
-import { Form, redirect, useActionData, useLoaderData, useNavigation } from 'react-router-dom';
+import {
+  Form,
+  redirect,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from 'react-router-dom';
 import { loginUser } from '../api';
+import styles from './Login.module.css';
 
 function Login() {
   const message = useLoaderData();
-  const error = useActionData()
-  const {state} = useNavigation()
-  const isSubmitting = state === "submitting"
+  const error = useActionData();
+  const { state } = useNavigation();
+  const isSubmitting = state === 'submitting';
 
   return (
-    <div className='login-container'>
-      <h1>Sign in to your account</h1>
-      {message && <h4 className='red'>{message}</h4>}
-      {error && <h4 className='red'>{error}</h4>}
+    <section className={styles.login}>
+      <div className={styles.container}>
+        <h1 className='title'>Sign in to your account</h1>
+        {message && <h4 className='red'>{message}</h4>}
+        {error && <h4 className='red'>{error}</h4>}
 
-      <Form method='post' className='login-form' replace >
-        <input name='email' type='email' placeholder='Email address' />
-        <input name='password' type='password' placeholder='Password' />
-        <button disabled={isSubmitting}>{isSubmitting ? 'Logging in...' : 'Login in'}</button>
-      </Form>
-    </div>
+        <Form method='post' className={styles.form} replace>
+          <input name='email' type='email' placeholder='Email address' />
+          <input name='password' type='password' placeholder='Password' />
+          <button disabled={isSubmitting}>
+            {isSubmitting ? 'Logging in...' : 'Login in'}
+          </button>
+        </Form>
+      </div>
+    </section>
   );
 }
 
@@ -32,8 +43,9 @@ async function action({ request }) {
   const email = formData.get('email');
   const password = formData.get('password');
 
-  const pathname = new URL(request.url).searchParams.get("redirectTo") || "/host"
-  
+  const pathname =
+    new URL(request.url).searchParams.get('redirectTo') || '/host';
+
   try {
     await loginUser({ email, password });
     localStorage.setItem('loggedin', true);
@@ -41,7 +53,7 @@ async function action({ request }) {
     response.body = true;
     return response;
   } catch (error) {
-    return error.message
+    return error.message;
   }
 }
 
